8e5fe313a5314cf6274c2dcfdb66d02eb1c2e856
BugHunter-X <askzenai@gmail.com>
2024-12-31 15:20:05 +0100

    Add DimensionsBug.js file

---
+++ DimensionsBug.js
@@ -0,0 +1,1 @@
+This error occurs when using the `Dimensions` API in React Native to get screen dimensions, but the dimensions are not updating correctly when the screen orientation changes. This can lead to layout issues and unexpected behavior in your app.
